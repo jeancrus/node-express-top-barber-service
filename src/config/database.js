95b7a4bd8573dbb6f78b33be26c61ee50e5f55b7
Jean Crus <jean.crus@outlook.com>
2020-02-02 12:30:17 -0300

    Express barber project

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'postgres',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
